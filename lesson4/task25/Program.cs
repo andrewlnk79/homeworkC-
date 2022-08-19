b77fa2e70bc238451a083e6e5c2fdb6bc364d392
AndreyLinnik <av.linnik79@ya.ru>
2022-08-19 18:35:39 +0300

    lesson4

---
+++ lesson4/task25/Program.cs
@@ -0,0 +1,13 @@
+﻿int exponentiation(int num1, int num2)
+{
+    int result = num1;
+    for (int i = 1; i < num2; i++)
+    {
+        result = result * num1;
+        // System.Console.WriteLine(result);
+
+
+    }
+    return result;
+}
+System.Console.WriteLine(exponentiation(3, 5));
